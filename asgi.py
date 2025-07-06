88d34d1852e52645efcbd8c9941a45e5a1c6bc76
CharlesLIU <liushaoqiangol@163.com>
2025-07-06 23:14:22 +0800

    Revise asgi.py.

--- asgi.py
+++ asgi.py
@@ -1,12 +1,11 @@
 import os
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from otree.asgi import application as otree_asgi_application
+from otree.channels import consumers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(otree_asgi_application),
+    "websocket": consumers.WebSocketEndpoint, 
 })
